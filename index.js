12fb6a12ab57426d7c9fbbc04e1997d2efe292da
HariKrishnan H <harikrishnan.h@diagnal.com>
2022-06-18 11:51:36 +0530

    Initial Commit.

---
+++ index.js
@@ -0,0 +1,109 @@
+import { useEffect, useRef } from 'react';
+// import { isKeyBackButton } from '../utils/commonUtils';
+
+const callBackFunctions = {};
+let callBackIDStack = [];
+
+let backButtons = [];
+
+export const initialize = ({ keyCodes = []}) => backButtons = keyCodes;
+
+const isKeyBackButton = (e) => backButtons.indexOf(e.keyCode) !== -1;
+
+/* --------------------------------- NOTICE ------------------------------------
+  The callBack function passed to backButtonController should return true if
+  the propogation of function to be terminated at that level. By deafult it will
+  keep on progating through all callBack Functons registered.
+  ------------------------------------------------------------------------------ */
+
+const getRandomIDArray = () => callBackIDStack.map((i) => i.randomID) || [];
+
+const updateCallBackIDStackwithRank = (randomID, rank) => {
+  let tempRankArray = callBackIDStack.map((i) => i.rank) || [];
+  const filteredRankArrr = [];
+  const filteredIDStack = [];
+
+  const rankFilter = (i) => {
+    const correspondingRankItem = tempRankArray[i] || null;
+    const correspondingIdItem = callBackIDStack[i] || null;
+    if (correspondingRankItem !== null) {
+      filteredRankArrr.unshift(correspondingRankItem);
+      filteredIDStack.unshift(correspondingIdItem);
+      rankFilter(i - 1);
+    }
+  };
+  rankFilter(tempRankArray.length - 1);
+
+  filteredRankArrr.push(rank); // Adding new rank
+  filteredIDStack.push({ randomID, rank }); // Adding new ID Object
+
+  const sortedRankArray = filteredRankArrr.sort((a, b) => a - b);
+  const newSortedIdArray = [];
+  sortedRankArray.forEach((Rank) => {
+    newSortedIdArray.push(filteredIDStack.filter(({ rank: r }) => r === Rank)[0]);
+  });
+
+  const croppedIDArray = callBackIDStack.slice(0, (callBackIDStack.length - newSortedIdArray.length) + 1);
+  const croppedRankArr = tempRankArray.slice(0, (tempRankArray.length - sortedRankArray.length) + 1); // Need only for debugging
+  callBackIDStack = [...croppedIDArray, ...newSortedIdArray];
+  tempRankArray = [...croppedRankArr, ...sortedRankArray]; // Need only for debugging
+};
+
+/** ------------------------ */
+
+const addNewCallbackToStack = ({ randomID, callBack = () => {}, rank = null } = {}) => {
+  if (getRandomIDArray().indexOf(randomID) === -1) {
+    const lastCallbackIDStackItem = callBackIDStack[callBackIDStack.length - 1];
+    if (rank && lastCallbackIDStackItem && lastCallbackIDStackItem.rank !== null) {
+      updateCallBackIDStackwithRank(randomID, rank);
+    } else {
+      callBackIDStack.push({ randomID, rank });
+    }
+  }
+  callBackFunctions[randomID] = callBack;
+};
+
+const removeCallBack = (randomID) => {
+  if (!randomID) {
+    callBackIDStack = [];
+  }
+  const indexOfCurrentCallback = getRandomIDArray().indexOf(randomID);
+  if (indexOfCurrentCallback > -1) {
+    callBackIDStack.splice(indexOfCurrentCallback, 1);
+    delete callBackFunctions[randomID];
+  }
+};
+
+const executeBackPressFunctionality = (parentLevel = 0) => {
+  const index = getRandomIDArray().length - (parentLevel + 1);
+  const callBackFunctionKey = callBackIDStack[index] && callBackIDStack[index].randomID;
+  const backFunction = callBackFunctions[callBackFunctionKey];
+  let stopPropogation = false;
+  if (backFunction && typeof backFunction === 'function') {
+    stopPropogation = backFunction();
+    if (!(stopPropogation === true)) executeBackPressFunctionality(parentLevel + 1);
+  }
+};
+
+const handleBackPress = (event) => {
+  if (isKeyBackButton(event)) {
+    executeBackPressFunctionality();
+  }
+};
+
+document.addEventListener('keydown', handleBackPress);
+
+export const removeBackButtonHandler = () => {
+  document.removeEventListener('keydown', handleBackPress);
+};
+
+const useBackButton = ({ callBack, rank = null, randomID: rID }) => {
+  const randomID = useRef(rID || Math.random());
+  addNewCallbackToStack({
+    randomID: randomID.current,
+    callBack,
+    rank,
+  });
+  useEffect(() => () => { removeCallBack(randomID.current); }, []);
+};
+export default useBackButton;
